8420806bae8a70a9a816566fcdf7d12bb34a3eb4
AlfinurS <aaalfinur87@gmail.com>
2023-05-10 21:26:22 +0300

    adding dist

---
+++ assets/CartPage-f4ebcdbf.js
@@ -0,0 +1,1 @@
+import{d as e,_ as t,c as a,o}from"./index-0e296ca3.js";const n=e({name:"CartPage",components:{}});function r(c,s,p,_,f,i){return o(),a("div",null,"CartPage")}const l=t(n,[["render",r],["__file","D:/GB/vite-project/src/pages/CartPage.vue"]]);export{l as default};
