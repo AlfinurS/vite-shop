1e8be364e804f998e2081ab9da846a2ce9294d1a
AlfinurS <aaalfinur87@gmail.com>
2023-05-10 21:26:22 +0300

    adding dist

--- dist/assets/ProfilePage-372751c3.js
+++
@@ -1,1 +0,0 @@
-import{d as e,_ as o,c as n,o as r}from"./index-3f838c23.js";const t=e({name:"ProfilePage",components:{}});function a(c,s,f,i,p,l){return r(),n("div",null,"ProfilePage")}const P=o(t,[["render",a],["__file","D:/GB/vite-project/src/pages/ProfilePage.vue"]]);export{P as default};
