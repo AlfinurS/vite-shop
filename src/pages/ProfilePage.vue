1b0ddd012101e56c6486bffaf8cdb4350050fd72
AlfinurS <aaalfinur87@gmail.com>
2023-05-17 12:57:14 +0300

    страница регистрации

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -18,8 +18,70 @@
       </nav>
     </div>
   </section>
-
-  <RegistrationForm></RegistrationForm>
+  <section class="registration box">
+    <div class="registration__wrapper">
+      <RegistrationForm></RegistrationForm>
+      <div class="registration__content">
+        <h2 class="registration__titel">loyalty has its perks</h2>
+        <p class="registration__text">
+          Get in on the loyalty program where you can earn points and unlock
+          serious perks. Starting with these as soon as you join:
+        </p>
+        <ul class="registration__list">
+          <li class="registration__item">
+            <img
+              class="btn-plain-icon"
+              src="img/svg/check-mark.svg"
+              alt="check"
+              width="20"
+              height="16"
+            />
+            <span>15% off welcome offer</span>
+          </li>
+          <li class="registration__item">
+            <img
+              class="btn-plain-icon"
+              src="img/svg/check-mark.svg"
+              alt="check"
+              width="20"
+              height="16"
+            />
+            <span>Free shipping, returns and exchanges on all orders</span>
+          </li>
+          <li class="registration__item">
+            <img
+              class="btn-plain-icon"
+              src="img/svg/check-mark.svg"
+              alt="check"
+              width="20"
+              height="16"
+            />
+            <span>$10 off a purchase on your birthday</span>
+          </li>
+          <li class="registration__item">
+            <img
+              class="btn-plain-icon"
+              src="img/svg/check-mark.svg"
+              alt="check"
+              width="20"
+              height="16"
+            />
+            <span>Early access to products</span>
+          </li>
+          <li class="registration__item">
+            <img
+              class="btn-plain-icon"
+              src="img/svg/check-mark.svg"
+              alt="check"
+              width="20"
+              height="16"
+            />
+            <span>Exclusive offers & rewards</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
